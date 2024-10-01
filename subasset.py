54440c3f1f392e1f3ca482990fd7605bf59deb3d
krisFernz26 <kfernz2626@gmail.com>
2024-10-01 11:31:21 +0800

    Added comments

--- subasset.py
+++ subasset.py
@@ -5,10 +5,14 @@ assetfinder_file_name = "assetfinder.txt"
 compiled_file_name = "subdomains.txt"
 
 def run():
+    # Ask for the domain to enumerate
     domain = input("Enter domain: ")
+    # Run Sublist3r
     subprocess.run(['sublist3r', '-d', domain, '-o', 'sublister.txt'])
+    # Outupt assetfinder results to file
     with open(assetfinder_file_name, 'w') as f:
         subprocess.run(['assetfinder', '-subs-only', domain], stdout=f)
+    # Sort results from sublist3r and assetfinder to a file
     with open(compiled_file_name, 'w') as f:
         subprocess.run(['sort', '-u', sublister_file_name, assetfinder_file_name], stdout=f)
 
