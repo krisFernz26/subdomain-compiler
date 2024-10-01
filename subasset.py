f282cfa2bbe18a7f401e7aada3cc4a0289608f44
krisFernz26 <kfernz2626@gmail.com>
2024-10-01 11:29:53 +0800

    Simplified

--- subasset.py
+++ subasset.py
@@ -1,24 +1,16 @@
 import subprocess
-from pathlib import Path
 
-cwd = Path.cwd()
 sublister_file_name = "sublister.txt"
 assetfinder_file_name = "assetfinder.txt"
 compiled_file_name = "subdomains.txt"
 
-def run_sublister(domain: str):
+def run():
+    domain = input("Enter domain: ")
     subprocess.run(['sublist3r', '-d', domain, '-o', 'sublister.txt'])
-
-def run_assetfinder(domain: str):
     with open(assetfinder_file_name, 'w') as f:
         subprocess.run(['assetfinder', '-subs-only', domain], stdout=f)
-
-def sort():
     with open(compiled_file_name, 'w') as f:
         subprocess.run(['sort', '-u', sublister_file_name, assetfinder_file_name], stdout=f)
 
 
-domain = input("Enter domain: ")
-run_sublister(domain)
-run_assetfinder(domain)
-sort()
+run()
